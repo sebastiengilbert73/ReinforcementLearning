352fd3dc5539d37a4f81b7a0e0408c92db4b8b61
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2019-01-28 20:20:59 -0500

    QLearning.py: Added epsilon

--- QLearning.py
+++ QLearning.py
@@ -9,6 +9,7 @@ class Solver:
                  learningRate,
                  environment,  # Must implement .Step(action), .Reset()
                  defaultValue, # Fill value for the Q matrix. It should be higher than the penalty given by the environment for a neutral move, in order to favor exploration of unseen cases
+                 epsilonRampDownNumberOfEpisodes,
                  ):
         self.numberOfObservations = numberOfObservations
         self.numberOfActions = numberOfActions
@@ -17,6 +18,8 @@ class Solver:
         self.environment = environment
         self.Q = numpy.zeros([numberOfObservations, numberOfActions])
         self.Q.fill(defaultValue)
+        self.epsilon = 1.0
+        self.epsilonRampDownNumberOfEpisodes = epsilonRampDownNumberOfEpisodes
 
     def BestAction(self, observation):
         bestAction = numpy.argmax(self.Q[observation])
@@ -28,7 +31,12 @@ class Solver:
         rewardSum = 0
         observation = self.environment.Reset()
         while not done:
-            action = self.BestAction(observation)
+            # Determine if we move randomly or if we pick the best action
+            randomNbr = numpy.random.uniform()
+            if randomNbr > self.epsilon:
+                action = self.BestAction(observation)
+            else: # Random move
+                action = numpy.random.randint(self.numberOfActions)
             nextObservation, reward, done, info = self.environment.Step(action)
             self.Q[observation, action] += self.learningRate * \
                 (reward + self.gamma * numpy.max(self.Q[nextObservation]) - self.Q[observation, action])
@@ -41,3 +49,5 @@ class Solver:
             rewardSum = self.UpdateQWithOneEpisode()
             if writeToConsole:
                 print ("{}. rewardSum = {}".format(episode, rewardSum))
+            # Update epsilon
+            self.epsilon = max(1.0 - episode/self.epsilonRampDownNumberOfEpisodes, 0.0)
