3ad192dfcc18e09402e8585c52a828ed305e94d0
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2020-12-28 09:51:32 -0500

    algorithms/examples/: 1st commit

---
+++ algorithms/examples/policy_evaluation.py
@@ -0,0 +1,67 @@
+import logging
+import argparse
+import ReinforcementLearning.algorithms.policy as policy
+import ReinforcementLearning.environments as environments
+from ReinforcementLearning.environments import gridworld1
+import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--environment', help="The environment to use. Default: 'gridworld1'", default='gridworld1')
+parser.add_argument('--legalActionsAuthority', help="The authority that filters the legal actions. Default: 'AllActionsLegal'", default='AllActionsLegal')
+parser.add_argument('--policy', help="The policy. Default: 'random'", default='random')
+parser.add_argument('--gamma', help="The discount factor. Default: 0.9", type=float, default=0.9)
+parser.add_argument('--minimumChange', help="The minimum value change to keep iterating. Default: 0.01", type=float, default=0.01)
+parser.add_argument('--numberOfSelectionsPerState', help="The number of tried selections per state. Should be 1 for deterministic policies. Default: 100", type=int, default=100)
+parser.add_argument('--maximumNumberOfIterations', help="The maximum number of iterations. Default: 1000", type=int, default=1000)
+parser.add_argument('--initialValue', help="The initial value for all states. Default: 0", type=float, default=0)
+parser.add_argument('--randomSeed', help="The seed for the random module. Default: 0", type=int, default=0)
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s [%(levelname)s] %(message)s')
+random.seed(args.randomSeed)
+
+def main():
+    logging.info("policy_evaluation.py    main()   environment={}".format(args.environment))
+
+    # Create the environment
+    # It must implement gym.Env, ActionsSet(), StatesSet(), SetState(s)
+    environment = None
+    if args.environment.lower() == 'gridworld1':
+        environment = gridworld1.GridWorld1()
+    elif args.environment.lower() == 'gridworld2x2':
+        environment = gridworld1.GridWorld2x2()
+    else:
+        raise NotImplementedError("main(): Not implemented environment '{}'".format(args.environment))
+
+    # Create the authority to filter the legal actions
+    actions_set = environment.ActionsSet()
+    legal_actions_authority = None
+    if args.legalActionsAuthority.lower() == 'allactionslegal':
+        legal_actions_authority = policy.AllActionsLegalAuthority(actions_set)
+    else:
+        raise NotImplementedError("main(): Not implemented legal actions authority '{}'".format(args.legalActionsAuthority))
+
+    # Create the evaluated policy
+    evaluated_policy = None
+    if args.policy.lower() == 'random':
+        evaluated_policy = policy.Random(legal_actions_authority)
+    else:
+        raise NotImplementedError("main(): Not implemented policy '{}'".format(args.policy))
+
+    # Create the policy evaluator
+    policy_evaluator = policy.PolicyEvaluator(environment=environment,
+                                              gamma=args.gamma,
+                                              minimum_change=args.minimumChange,
+                                              number_of_selections_per_state=args.numberOfSelectionsPerState,
+                                              maximum_number_of_iterations=args.maximumNumberOfIterations,
+                                              initial_value=args.initialValue
+                                              )
+
+    # Evaluation
+    logging.info("Starting evaluation...")
+    state_to_value_dict = policy_evaluator.Evaluate(evaluated_policy)
+    logging.info("Done!")
+    print("state_to_value_dict = \n{}".format(state_to_value_dict))
+
+if __name__ == '__main__':
+    main()
