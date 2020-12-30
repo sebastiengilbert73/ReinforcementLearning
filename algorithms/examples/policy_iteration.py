701e8460e05fce922adea4c30fec57c378e69b3e
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2020-12-30 08:03:19 -0500

    policy_iteration.py: 1st commit

---
+++ algorithms/examples/policy_iteration.py
@@ -0,0 +1,80 @@
+import logging
+import argparse
+import ReinforcementLearning.algorithms.policy as rl_policy
+import ReinforcementLearning.environments as environments
+from ReinforcementLearning.environments import gridworlds
+from ReinforcementLearning.environments import jacks_car_rental
+import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--randomSeed', help="The seed for the random module. Default: 0", type=int, default=0)
+parser.add_argument('--environment', help="The environment to use. Default: 'gridworld1'", default='gridworld1')
+parser.add_argument('--legalActionsAuthority', help="The authority that filters the legal actions. Default: 'AllActionsLegal'", default='AllActionsLegal')
+#parser.add_argument('--policy', help="The policy. Default: 'random'", default='random')
+parser.add_argument('--gamma', help="The discount factor. Default: 0.9", type=float, default=0.9)
+parser.add_argument('--minimumChange', help="For the evaluator, the minimum value change to keep iterating. Default: 0.01", type=float, default=0.01)
+parser.add_argument('--numberOfSelectionsPerState', help="For the evaluator, the number of tried selections per state. Should be 1 for deterministic policies. Default: 100", type=int, default=100)
+parser.add_argument('--evaluatorMaximumNumberOfIterations', help="For the evaluator, the maximum number of iterations. Default: 1000", type=int, default=1000)
+parser.add_argument('--initialValue', help="The initial value for all states. Default: 0", type=float, default=0)
+#parser.add_argument('--epsilon', help="For epsilon-greedy policies, the probability of choosing a random action. Default: 0.1", type=float, default=0.1)
+parser.add_argument('--numberOfTrialsPerAction', help="The number of trials per action. For deterministic environments, should be 1. Default: 1", type=int, default=1)
+parser.add_argument('--iteratorMaximumNumberOfIterations', help="For the policy iterator, the maximum number of iterations. Default: 100", type=int, default=100)
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s [%(levelname)s] %(message)s')
+random.seed(args.randomSeed)
+
+def main():
+    logging.info("policy_iteration.py    main()   environment={}".format(args.environment))
+
+    # Create the environment
+    # It must implement gym.Env, ActionsSet(), StatesSet(), SetState(s)
+    environment = None
+    if args.environment.lower() == 'gridworld1':
+        environment = gridworlds.GridWorld1()
+    elif args.environment.lower() == 'gridworld2x2':
+        environment = gridworlds.GridWorld2x2()
+    elif args.environment.lower() == 'jackscarrental':
+        environment = jacks_car_rental.JacksCarRental()
+    else:
+        raise NotImplementedError("main(): Not implemented environment '{}'".format(args.environment))
+
+    # Create the authority to filter the legal actions
+    actions_set = environment.ActionsSet()
+    legal_actions_authority = None
+    if args.legalActionsAuthority.lower() == 'allactionslegal':
+        legal_actions_authority = rl_policy.AllActionsLegalAuthority(actions_set)
+    elif args.legalActionsAuthority.lower() == 'jackspossiblemoves':
+        legal_actions_authority = jacks_car_rental.JacksPossibleMoves()
+    else:
+        raise NotImplementedError("main(): Not implemented legal actions authority '{}'".format(args.legalActionsAuthority))
+
+    # Create the policy evaluator
+    policy_evaluator = rl_policy.PolicyEvaluator(environment=environment,
+                                              gamma=args.gamma,
+                                              minimum_change=args.minimumChange,
+                                              number_of_selections_per_state=args.numberOfSelectionsPerState,
+                                              maximum_number_of_iterations=args.evaluatorMaximumNumberOfIterations,
+                                              initial_value=args.initialValue
+                                              )
+
+    # Create the policy iterator
+    policy_iterator = rl_policy.PolicyIterator(
+         environment=environment,
+         policy_evaluator=policy_evaluator,
+         legal_actions_authority=legal_actions_authority,
+         gamma=args.gamma,
+         initial_value=args.initialValue,
+         number_of_trials_per_action=args.numberOfTrialsPerAction,
+         maximum_number_of_iterations=args.iteratorMaximumNumberOfIterations,
+         print_steps=True
+    )
+
+    # Iterate the policy
+    policy, policy_state_to_actions_probabilities = policy_iterator.IteratePolicy()
+
+    print("policy_state_to_actions_probabilities =\n{}".format(policy_state_to_actions_probabilities))
+
+
+if __name__ == '__main__':
+    main()
