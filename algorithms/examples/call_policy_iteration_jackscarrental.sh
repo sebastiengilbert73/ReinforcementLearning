da38ba3a66b056f0bdf88d7cc81413abfbbbcfc4
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2021-01-03 05:28:01 -0500

    call_policy_iteration_jackscarrental.sh: --evaluatorMaximumNumberOfIterations: 1000 -> 1

--- algorithms/examples/call_policy_iteration_jackscarrental.sh
+++ algorithms/examples/call_policy_iteration_jackscarrental.sh
@@ -7,7 +7,7 @@ python policy_iteration.py \
 	--epsilon=0.1 \
 	--minimumChange=50 \
 	--numberOfSelectionsPerState=1 \
-	--evaluatorMaximumNumberOfIterations=1000 \
+	--evaluatorMaximumNumberOfIterations=1 \
 	--initialValue=0 \
 	--numberOfTrialsPerAction=1 \
 	--iteratorMaximumNumberOfIterations=100 \
