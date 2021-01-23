35a14166033f06a38bdf05831197a7f86f6f4834
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2021-01-23 12:31:41 -0500

    blackjack.py: Blackjack and BlackjackES: Episodic; BlackjackES.step(): implementation

--- environments/blackjack.py
+++ environments/blackjack.py
@@ -6,8 +6,8 @@ import random
 import ReinforcementLearning.algorithms.policy as rl_policy
 import ReinforcementLearning.environments.attributes as env_attributes
 
-class Blackjack(env_attributes.GymCompatible,
-                env_attributes.Tabulatable):
+class Blackjack(env_attributes.Tabulatable,
+                env_attributes.Episodic):
     def __init__(self):
         self.blackjack_env = gym.make('Blackjack-v0')
         self.reset()
@@ -83,9 +83,9 @@ class HitUpTo(rl_policy.Policy):
             return {0: 1, 1: 0}  # Stand
 
 
-class BlackjackES(env_attributes.GymCompatible,
-                  env_attributes.Tabulatable,
-                  env_attributes.ExplorationStarts):
+class BlackjackES(env_attributes.Tabulatable,
+                  env_attributes.ExplorationStarts,
+                  env_attributes.Episodic):
     def __init__(self):
         super(env_attributes.GymCompatible).__init__()
         super(env_attributes.Tabulatable).__init__()
@@ -97,27 +97,107 @@ class BlackjackES(env_attributes.GymCompatible,
 
     def step(self, action):
         # return (observation, reward, done, info_dict)
+        info_dict = {'dealer_cards': [self.state[2]], 'player_card': None}
         if action == 0:  # stand
-            pass
+            dealer_card1 = self.state[2]
+            # Dealer gets a card
+            dealer_card2 = random.randint(1, 13)
+            info_dict['dealer_cards'].append(dealer_card2)
+            if dealer_card2 >= 10:
+                dealer_card2 = 10
+            dealer_sum = dealer_card1 + dealer_card2
+            dealer_has_usable_ace = False
+            if dealer_card1 == 1 or dealer_card2 == 1:
+                dealer_has_usable_ace = True
+                dealer_sum += 10
+            dealer_busted = False
+            while not dealer_busted and dealer_sum <= 17:
+                dealer_card = random.randint(1, 13)
+                info_dict['dealer_cards'].append(dealer_card)
+                if dealer_card >= 10:
+                    dealer_card = 10
+                if dealer_card == 1:
+                    if dealer_sum > 10:
+                        dealer_sum += 1
+                    else:
+                        dealer_sum += 11
+                        dealer_has_usable_ace = True
+                else:
+                    dealer_sum += dealer_card
+
+                if dealer_sum > 21:
+                    if dealer_has_usable_ace:
+                        dealer_sum -= 10
+                        dealer_has_usable_ace = False
+                    else:
+                        dealer_busted = True
+            reward = 0  # Draw
+            if dealer_busted:
+                reward = 1
+            else:
+                if self.state[0] > dealer_sum:
+                    reward = 1
+                elif self.state[0] < dealer_sum:
+                    reward = -1
+            self.state = (self.state[0], self.state[1], dealer_sum)
+            return (self.state, reward, True, info_dict)
+        elif action == 1:  # hit
+            player_sum = self.state[0]
+            if player_sum > 21:
+                raise ValueError("BlackjackES.step(): player_sum ({}) > 21".format(player_sum))
+            player_has_usable_ace = self.state[1]
+            new_card = random.randint(1, 13)
+            info_dict['player_card'] = new_card
+            if new_card >= 10:
+                new_card = 10
+            done = False
+            reward = 0
+            if new_card == 1:
+                if player_sum > 10:
+                    player_sum += 1
+                else:
+                    player_sum += 11
+                    player_has_usable_ace = True
+            else:
+                player_sum += new_card
+            if player_sum > 21:
+                if player_has_usable_ace:
+                    player_sum -= 10
+                    player_has_usable_ace = False
+                else:
+                    done = True
+                    reward = -1
+            self.state = (player_sum, player_has_usable_ace, self.state[2])
+            return (self.state, reward, done, info_dict)
+        else:
+            raise ValueError("BlackjackES.step(): action = {}. It should be 0 for stand or 1 for hit".format(action))
 
     def reset(self):
+        info_dict = {'player_cards': [], 'dealer_card': None}
         usable_ace = False
-        card1 = random.randint(1, 11)
-        card2 = random.randint(1, 11)
-        if card1 == 11 or card2 == 11:
-            usable_ace = True
+        card1 = random.randint(1, 13)
+        info_dict['player_cards'].append(card1)
+        if card1 >= 10:
+            card1 = 10
+        card2 = random.randint(1, 13)
+        info_dict['player_cards'].append(card2)
+        if card2 >= 10:
+            card2 = 10
         sum = card1 + card2
-        if sum == 22:  # two aces
-            sum = 12
-
-        dealer_card = random.randint(1, 11)
+        if card1 == 1 or card2 == 1:
+            usable_ace = True
+            sum += 10
+        dealer_card = random.randint(1, 13)
+        info_dict['dealer_card'] = dealer_card
+        if dealer_card >= 10:
+            dealer_card = 10
         self.state = (sum, usable_ace, dealer_card)
         done = False
         reward = 0
         if sum == 21:
             done = True
             reward = 1
-        return (self.state, reward, done, {})
+        return (self.state, reward, done, info_dict)
 
     def render(self, mode):
         print(self.state)
